bb259158975f10a7f65325b6c8b4822036059cc8
emralveli <55778495+emralveli@users.noreply.github.com>
2022-11-17 13:06:39 +0200

    Story 2 comments and ideas

---
+++ src/main/java/FdibamonFight.java
@@ -0,0 +1,12 @@
+public class FdibamonFight {
+    int round = 0;
+
+    
+    //attack function where you subtract the attack power of one fdon from the other fdibamons hitpoints - increment round in this function (?)
+    // - in each round, each fdibamon is a defender and an attacker at the same time, so the attack goes both ways (they both lose hp)
+    //print function that shows the aftermath of the attack (i.e. the hitpoints of the fdibamons, their names and so on)
+    // (?) seperate function for health check (hp can't be a value lower than 0)
+    //function to determine the winner of the fight (the fdibamon that hits 0 hitpoints first is the loser, thus the other one is the winner)
+    // - if both hit 0 in the same round - the fight is a draw
+    // - print the result (maybe with the remaining hp as well?)
+}
